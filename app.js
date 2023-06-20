86c722477d82cd0186b872fad65b5ea7f417fa24
lproko <l.proko@hypertech.gr>
2023-06-20 17:15:29 +0300

    UI & select countries

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-let height = 1200;
-let width = 1200;
+let height = 500;
+let width = 1300;
 let margin = { top: 10, right: 40, bottom: 34, left: 40 };
+
 
 // Data structure describing chart scales
 let Scales = {
@@ -10,8 +11,8 @@ let Scales = {
 
 // Data structure describing volume of displayed data
 let Count = {
-  total: "age",
-  perCap: "country",
+  total: "Default view",
+  perCap: "Member states",
 };
 
 // Data structure describing legend fields value
@@ -37,7 +38,7 @@ let colors = d3
     "southAmerica",
     "oceania",
   ])
-  .range(["#D81B60", "#1976D2", "#388E3C", "#FBC02D", "#E64A19", "#455A64"]);
+  .range(["#D81B60", "#1976D2", "#388E3C", "#BCBCBC", "#E64A19", "#455A64"]);
 
 d3.select("#asiaColor").style("color", colors("asia"));
 d3.select("#africaColor").style("color", colors("africa"));
@@ -50,20 +51,21 @@ let svg = d3
   .select("#svganchor")
   .append("svg")
   .attr("width", width)
-  .attr("height", height);
+  .attr("height", height)
+  
 
-let xScale = d3.scaleLinear().range([margin.left, width - margin.right]);
-let yScale = d3.scaleLinear();
+let xScale = d3.scaleLinear().range([margin.left+50, width - margin.right-50]);
+let yScale = d3.scalePoint();
 
 svg
   .append("g")
   .attr("class", "x axis")
-  .attr("transform", "translate(0," + (height - margin.bottom) + ")");
+  .attr("transform", "translate(0," + (margin.top+10) + ")");
 
 svg
   .append("g")
   .attr("class", "y axis")
-  .attr("transform", "translate(" + (margin.left - 5) + ",-5)");
+  .attr("transform", "translate(" + (width) + ",0)");
 
 // Create line that connects circle and X,Y axis
 let xLine = svg
@@ -93,8 +95,8 @@ let tooltip2 = d3
 d3.csv("data/mep-data.csv")
   .then(function (data) {
     let dataSet = data;
-    initialize()
-     redraw();
+    initialize();
+    redraw();
 
     // Listen to click on "total" and "per capita" buttons and trigger redraw when they are clicked
     d3.selectAll(".measure").on("click", function () {
@@ -110,7 +112,7 @@ d3.csv("data/mep-data.csv")
     });
 
     // Trigger filter function whenever checkbox is ticked/unticked
-    d3.selectAll("input").on("change", filter);
+    d3.selectAll("select").on("change", filter);
 
     function initialize() {
       // Create simulation with specified dataset
@@ -122,12 +124,12 @@ d3.csv("data/mep-data.csv")
           d3
             .forceX(function (d) {
               // Mapping of values from age/country column of dataset to range of SVG chart (<margin.left, margin.right>)
-              return xScale(+d[chartState.measure]); // This is the desired position
+              return xScale(+d["age"] ); // This is the desired position
             })
             .strength(2)
         ) // Increase velocity
-        .force("y", d3.forceY(height / 2 - margin.bottom / 2)) // // Apply positioning force to push nodes towards center along Y axis
-        .force("collide", d3.forceCollide(8)) // Apply collision force with radius of 8 - keeps nodes centers 8 pixels apart
+        .force("y", d3.forceY(height / 2 - margin.bottom / 2  )) // // Apply positioning force to push nodes towards center along Y axis
+        //.force("collide", d3.forceCollide(8)) // Apply collision force with radius of 8 - keeps nodes centers 8 pixels apart
         .stop(); // Stop simulation from starting automatically
 
       // Manually run simulation
@@ -142,13 +144,7 @@ d3.csv("data/mep-data.csv")
           return d.country;
         });
 
-      countriesCircles
-        .exit()
-        .transition()
-        .duration(1000)
-        .attr("cx", 0)
-        .attr("cy", height / 2 - margin.bottom / 2)
-        .remove();
+      
       // fill bubles with color based on whatever we declare
       countriesCircles
         .enter()
@@ -156,7 +152,7 @@ d3.csv("data/mep-data.csv")
         .attr("class", "countries")
         .attr("cx", 0)
         .attr("cy", height / 2 - margin.bottom / 2)
-        .attr("r", 4)
+        .attr("r", 3)
         .attr("fill", function (d) {
           return colors("europe");
         })
@@ -174,42 +170,49 @@ d3.csv("data/mep-data.csv")
 
     function redraw() {
       if (chartState.measure == Count.total) {
-        xScale = d3.scaleLinear().range([margin.left, width - margin.right]);
+        xScale = d3.scaleLinear().range([margin.left+50, width - margin.right-50]);
+        yScale = d3.scalePoint().range([height-margin.bottom, margin.top+50]);
         xScale.domain(
           d3.extent(dataSet, function (d) {
-            return +d[chartState.measure];
+            return +d["age"];
           })
         );
 
         let xAxis;
-
+        let yAxis;
         //  Set X axis based on new scale.
 
-        xAxis = d3.axisBottom(xScale);
+        yScale.domain(['All']);
 
+        xAxis = d3.axisTop(xScale).tickSizeOuter(0);
+        yAxis = d3.axisLeft(yScale).tickSize(width-margin.right).tickSizeOuter(0);
         d3.transition(svg)
           .select(".x.axis")
           .transition()
           .duration(1000)
           .call(xAxis);
-
         
-          
-          
-
-        
+          d3.transition(svg)
+          .select(".y.axis")
+          .transition()
+          .duration(1000)
+          .call(yAxis)
+          //.attr("transform", "translate(" + (margin.left+10 ) + ",0)");
 
         // Create simulation with specified dataset
         let simulation = d3
           .forceSimulation(dataSet)
           //  Apply positioning force to push nodes towards desired position along X axis
-          .force("x", d3.forceX(function (d) {
-                // Mapping of values from age/country column of dataset to range of SVG chart (<margin.left, margin.right>)
-                return xScale(+d[chartState.measure]); // This is the desired position
-              }))
-               // Increase velocity
-          .force("y", d3.forceY(height / 2 - margin.bottom / 2)) // // Apply positioning force to push nodes towards center along Y axis
-          .force("collide", d3.forceCollide(8)) // Apply collision force with radius of 8 - keeps nodes centers 8 pixels apart
+          .force(
+            "x",
+            d3.forceX(function (d) {
+              // Mapping of values from age/country column of dataset to range of SVG chart (<margin.left, margin.right>)
+              return xScale(+d["age"]); // This is the desired position
+            })
+          )
+          // Increase velocity
+          .force("y", d3.forceY(height / 2 + margin.bottom/2 )) // // Apply positioning force to push nodes towards center along Y axis
+          .force("collide", d3.forceCollide(5)) // Apply collision force with radius of 8 - keeps nodes centers 8 pixels apart
           .stop(); // Stop simulation from starting automatically
 
         // Manually run simulation
@@ -240,28 +243,29 @@ d3.csv("data/mep-data.csv")
           .on("mousemove", function (d) {
             tooltip
               .html(
-                `Country: <strong>${d.country}</strong><br>
-                          ${chartState.legend.slice(
-                            0,
-                            chartState.legend.indexOf(",")
-                          )}: 
-                          <strong>${d3.format(",")(
-                            d[chartState.measure]
-                          )}</strong>
-                          ${chartState.legend.slice(
-                            chartState.legend.lastIndexOf(" ")
-                          )}`
+                `
+                  <div style="height:4px;width:100%;" class="red-line"></div>
+                  <div class="container">
+                  <div class="name"><h2>${d["mep_honorific_prefix"]} ${d["mep_given_name"]} ${d["mep_family_name"]}</h2></div><hr/>
+                  <div style="display:flex;" class="elected">
+                    <div style="text-align: left;width:50%;"><h5>Elected by</h5></div>
+                    <div style="text-align: right;width:50%;"><h5><strong>${d["country"]}</strong></h5></div>
+                  </div><hr/>
+                <div style="display:flex;" class="party">
+                  <div style="text-align: left;width:50%;"><h5>Party</h5></div>
+                  <div style="text-align: right;width:50%;"><h5><strong>${d["political_group_abb"]}</strong></h5></div>
+                </div><hr/>
+                <div style="display:flex;" class="age">
+                  <div style="text-align: left;width:50%;"><h5>Age</h5></div>
+                  <div style="text-align: right;width:50%;"><h5><strong>${d["age"]}</strong></h5></div>
+                </div><hr/>
+                </div>`
               )
               .style("top", d3.event.pageY - 12 + "px")
               .style("left", d3.event.pageX + 25 + "px")
-              .style("opacity", 0.9);
+              .style("opacity", 1);
 
-            xLine
-              .attr("x1", d3.select(this).attr("cx"))
-              .attr("y1", d3.select(this).attr("cy"))
-              .attr("y2", height - margin.bottom)
-              .attr("x2", d3.select(this).attr("cx"))
-              .attr("opacity", 1);
+            
           })
           .on("mouseout", function (_) {
             tooltip.style("opacity", 0);
@@ -270,22 +274,23 @@ d3.csv("data/mep-data.csv")
       }
 
       if (chartState.measure === Count.perCap) {
-        yScale = d3.scaleLinear().range([height - margin.bottom, margin.top]);
+        
+        yScale = d3.scalePoint().range([height-margin.bottom, margin.top+50]);
         xScale.domain(
           d3.extent(dataSet, function (d) {
             return +d["age"];
           })
         );
         let countries = [...new Set(dataSet.map((d) => d.country))];
-        console.log(countries);
-        yScale.domain([0, countries.length]);
+        yScale.domain(countries);
 
         let xAxis;
         let yAxis;
         // Set X axis based on new scale. If chart is set to "per capita" use numbers with one decimal point
 
-        xAxis = d3.axisBottom(xScale);
-        yAxis = d3.axisLeft(yScale);
+        xAxis = d3.axisTop(xScale).tickSizeOuter(0);
+        
+        yAxis = d3.axisLeft(yScale).tickSize(width-margin.right-margin.left).tickSizeOuter(0);
         d3.transition(svg)
           .select(".x.axis")
           .transition()
@@ -294,6 +299,7 @@ d3.csv("data/mep-data.csv")
 
         d3.transition(svg)
           .select(".y.axis")
+          .attr("transform", "translate(" + (width) + ",0)")
           .transition()
           .duration(1000)
           .call(yAxis);
@@ -302,34 +308,31 @@ d3.csv("data/mep-data.csv")
         let simulation = d3
           .forceSimulation(dataSet)
           // Apply positioning force to push nodes towards desired position along X axis
-          
+
           .force(
             "x",
-            d3
-              .forceX(function (d) {
-                // Mapping of values from age/country column of dataset to range of SVG chart (<margin.left, margin.right>)
-                return xScale(+d["age"]); // This is the desired position
-              })
-              
+            d3.forceX(function (d) {
+              // Mapping of values from age/country column of dataset to range of SVG chart (<margin.left, margin.right>)
+              return xScale(+d["age"]); // This is the desired position
+            })
           )
           //.force("collide", d3.forceCollide(8)) // Increase velocity
           .force(
             "y",
             d3.forceY(function (d, index) {
-              var temp = countries.indexOf(d["country"]);
-              console.log(temp);
-              return yScale(temp); // This is the desired position
+              return yScale(d["country"]); // This is the desired position
             })
           )
           .force("collide", d3.forceCollide(4)) // // Apply positioning force to push nodes towards center along Y axis
-          
+
           .stop(); // Stop simulation from starting automatically
 
         // Manually run simulation
         for (let i = 0; i < dataSet.length; ++i) {
           simulation.tick();
+          
         }
-
+        ;
         // Create country circles
         let countriesCircles = svg
           .selectAll(".countries")
@@ -345,40 +348,40 @@ d3.csv("data/mep-data.csv")
             return d.x;
           })
           .attr("cy", function (d) {
-            return d.y;
+            
+              return d.y;
           });
-
+          
+          
         // Show tooltip when hovering over circle (data for respective country)
         d3.selectAll(".countries")
           .on("mousemove", function (d) {
             tooltip
-              .html(
-                `Country: <strong>${d["country"]}</strong><br>
-                      ${chartState.legend.slice(
-                        0,
-                        chartState.legend.indexOf(",")
-                      )}: 
-                      <strong>${d3.format(",")(d["age"])}</strong>
-                      ${chartState.legend.slice(
-                        chartState.legend.lastIndexOf(" ")
-                      )}`
-              )
+            .html(
+              `
+                <div style="height:4px;width:100%;" class="red-line"></div>
+                <div class="container">
+                <div class="name"><h2>${d["mep_honorific_prefix"]} ${d["mep_given_name"]} ${d["mep_family_name"]}</h2></div><hr/>
+                <div style="display:flex;" class="elected">
+                  <div style="text-align: left;width:50%;"><h5>Elected by</h5></div>
+                  <div style="text-align: right;width:50%;"><h5><strong>${d["country"]}</strong></h5></div>
+                </div><hr/>
+              <div style="display:flex;" class="party">
+                <div style="text-align: left;width:50%;"><h5>Party</h5></div>
+                <div style="text-align: right;width:50%;"><h5><strong>${d["political_group_abb"]}</strong></h5></div>
+              </div><hr/>
+              <div style="display:flex;" class="age">
+                <div style="text-align: left;width:50%;"><h5>Age</h5></div>
+                <div style="text-align: right;width:50%;"><h5><strong>${d["age"]}</strong></h5></div>
+              </div><hr/>
+              </div>`
+            )
               .style("top", d3.event.pageY - 12 + "px")
               .style("left", d3.event.pageX + 25 + "px")
-              .style("opacity", 0.9);
+              .style("opacity", 1);
 
-            xLine
-              .attr("x1", d3.select(this).attr("cx"))
-              .attr("y1", d3.select(this).attr("cy"))
-              .attr("y2", height - margin.bottom)
-              .attr("x2", d3.select(this).attr("cx"))
-              .attr("opacity", 1);
-            yLine
-              .attr("x1", d3.select(this).attr("cx"))
-              .attr("y1", d3.select(this).attr("cy"))
-              .attr("y2", height - margin.bottom)
-              .attr("x2", d3.select(this).attr("cx"))
-              .attr("opacity", 1);
+            
+              
           })
           .on("mouseout", function (_) {
             tooltip.style("opacity", 0);
@@ -391,22 +394,27 @@ d3.csv("data/mep-data.csv")
     // Filter data based on which checkboxes are ticked
 
     function filter() {
+      console.log("eimai sto filter")
       function getCheckedBoxes(checkboxName) {
+        
         let checkboxes = d3.selectAll(checkboxName).nodes();
+        
         let checkboxesChecked = [];
-        for (let i = 0; i < checkboxes.length; i++) {
-          if (checkboxes[i].checked) {
-            checkboxesChecked.push(checkboxes[i].defaultValue);
+        for (let i = 0; i < 27; i++) {
+          
+          if (checkboxes[i].selected) {
+            checkboxesChecked.push(checkboxes[i].value);
           }
         }
         return checkboxesChecked.length > 0 ? checkboxesChecked : null;
       }
 
-      let checkedBoxes = getCheckedBoxes(".continent");
+      let checkedBoxes = getCheckedBoxes(".country");
 
       let newData = [];
 
       if (checkedBoxes == null) {
+        console.log("eimai emptyyyy")
         dataSet = newData;
         redraw();
         return;
@@ -414,7 +422,8 @@ d3.csv("data/mep-data.csv")
 
       for (let i = 0; i < checkedBoxes.length; i++) {
         let newArray = data.filter(function (d) {
-          return d.continent === checkedBoxes[i];
+          console.log(checkedBoxes[i])
+          return d.country === checkedBoxes[i];
         });
         Array.prototype.push.apply(newData, newArray);
       }
